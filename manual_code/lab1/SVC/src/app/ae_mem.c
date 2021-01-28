27acfccc589426c1008dc7c372782b7089026891
Indraj Kang <is2kang@uwaterloo.ca>
2021-01-28 15:13:46 -0500

    throughput tests and dealloc/malloc tests added

--- manual_code/lab1/SVC/src/app/ae_mem.c
+++ manual_code/lab1/SVC/src/app/ae_mem.c
@@ -54,6 +54,8 @@ int test_mem(void) {
         result |= BIT(3);
     }
     
+    
+    
     // start of k_mem_count_extfrag testing
 
     p[0] = k_mem_alloc(8);
@@ -88,6 +90,56 @@ int test_mem(void) {
     if (k_mem_count_extfrag(256) == 0) {
     	result |= BIT(8);
     }
+    
+    //testing throughput and heap utilization
+    //start time is 1 second and we are running 3200 alloc/dealloc operations");
+   for(int i =0; i < 40; i++)    {
+    void *p[40];
+    for (int j =0; j<40; j++) {
+    	p[j] = mem_alloc(8);
+
+    }
+
+    for (int k = 0; k<40; k++) {
+    	mem_dealloc(p[k]);
+    }
+
+
+   }
+	//loop ends at 45 seconds, 3200/45 is 71. Our throughput is 71.
+	
+	
+	//testing random de allocs to make sure it's coalescing 
+	void *p[8];
+	
+	 p[0] = mem_alloc(8);
+     p[1] = mem_alloc(8);
+     p[2] = mem_alloc(8);
+    p[3] = mem_alloc(8);
+    p[4] = mem_alloc(8);
+    p[5] = mem_alloc(8);
+    p[6] = mem_alloc(8);
+    p[7] = mem_alloc(8);
+
+    mem_dealloc(p[7]);
+    if (mem_count_extfrag(128) == 0) result  |= BIT(9);
+    mem_dealloc(p[5]);
+    if(mem_count_extfrag(128) == 1)  result |= BIT(10);
+    mem_dealloc(p[6]);
+    if (mem_count_extfrag(128) == 0) result  |= BIT(11);
+    mem_dealloc(p[3]);
+    if(mem_count_extfrag(128) == 1)  result |= BIT(12);
+    mem_dealloc(p[0]);
+    if(mem_count_extfrag(128) == 2)  result |= BIT(13);
+    mem_dealloc(p[2]);
+    if(mem_count_extfrag(32) == 1) result |= BIT(14);
+    mem_dealloc(p[1]);
+    if(mem_count_extfrag(48) == 0) result |= BIT(15);
+    if(mem_count_extfrag(128) == 1)  result |= BIT(16);
+    mem_dealloc(p[4]);
+    if (mem_count_extfrag(128) == 0) result  |= BIT(17);
+    
+    
     
     return result;
 }
